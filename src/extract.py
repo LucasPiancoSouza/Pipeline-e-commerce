b3f68ff4f6d93ca3796140f74283d2b6f1b9298f
Lucas Piancó <lucaspianco133@gmail.com>
2026-07-14 23:33:08 -0300

    Começando os primeiros passos do projeto

---
+++ src/extract.py
@@ -0,0 +1,33 @@
+#Biblioteca python para trabalhar com caminhos de pastas,
+#o pathlib ele representa caminhos como objetos
+from pathlib import Path
+import pandas as pd
+
+PASTA_DADOS = Path("data/raw")
+
+#O nome das coisas deve explicar a sua responsabilidade
+def encontrar_arquivos_csv(pasta:Path) -> list[Path]:
+    #pasta:Path - espera receber um objeto Path
+    #list[Path] - esta função retorna uma lista de objetos Path
+    return list(pasta.glob("*.csv"))
+
+def carregar_csvs(arquivos: list[Path]) -> tuple[ dict[str, pd.DataFrame],dict[Path, str]]:
+
+    erros: dict[Path, str] = {}
+    dataframes: dict[str, pd.DataFrame] = {} 
+
+    for arquivo in arquivos:
+# Tenta carregar o arquivo CSV
+        nome = arquivo.stem
+        nome = nome.replace("olist_", "")
+        nome = nome.replace("_dataset", "")
+        try:
+            df = pd.read_csv(arquivo)
+            dataframes[nome] = df
+
+# Caso ocorra algum erro durante a leitura, registra o erro
+        except Exception as erro:
+            erros[arquivo] = str(erro) 
+
+    return dataframes, erros
+
--- src/main.py
+++
@@ -1,4 +0,0 @@
-import pandas as pd
-import glob 
-
-csv_files = glob.glob('../data/*.csv')
